500e43fb53534575fdb1e099c68155ad2cc1cc51
Dylan Purches <dylan.purches@ext.ons.gov.uk>
2021-04-26 16:24:42 +0000

    Added data to pubsub upload

--- cf/xbrl_web_scraper/main.py
+++ cf/xbrl_web_scraper/main.py
@@ -52,6 +52,7 @@ def scrape_webpage(event, context):
     if not eval(test_run):
         publisher = pubsub_v1.PublisherClient()
         topic_path = publisher.topic_path("ons-companies-house-dev", "downloaded_zip_files")
+        data = f"Triggering unpacker for {link}"
         future = publisher.publish(
-            topic_path, zip_path=dir_to_save+"/"+link
+            topic_path, data, zip_path=dir_to_save+"/"+link
         )
