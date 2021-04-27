d88f754f4a95e4ec5fd9641cd11d0ff07d8681bc
Kirsty228 <63724065+Kirsty228@users.noreply.github.com>
2021-04-27 11:44:37 +0100

    Update main.py
    Doc strings and comments etc have been reviewed and updated where necessary.  Line length may not comply with convention as reviewing in GitHub.
    Except:
    Type hinting (line 10) needs completion.
    For the trigger, is there a specific layout to follow?

--- cf/xbrl_web_scraper/main.py
+++ cf/xbrl_web_scraper/main.py
@@ -1,30 +1,36 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-import time
-import random
 import base64
+from bs4 import BeautifulSoup
+import os
+import random
+import requests
+import time
+
 from google.cloud import storage
 
-def scrape_webpage(event, context):
+def scrape_webpage(event, context): # -> blob?
     """
+    Cloud Function to be triggered by Pub/Sub.
     Downloads a link specified as a message attibute and saves to a 
-    GCS location again specified by a message attribute.
+    Google Cloud Storage (GCS) location again specified by a message
+    attribute.
 
     Arguments:
-        event (dict): Event payload.
-        context (google.cloud.functions.Context): Metadata for the event.
+        event (dict): The dictionary with data specific to this type
+                      of event (the event payload).
+        context (google.cloud.functions.Context): The Cloud Functions
+                      event metadata (the triggering event).
     Returns:
         None
     Raises:
         None
     """
-    # Specify the bucket where .zip files should be saved
+    # Specifies the bucket where the .zip files should be saved.
     dir_to_save = "ons-companies-house-dev-xbrl-scraped-data"
 
-    # Set up a GCS client to handle the download to GCS
+    # Sets up a GCS client to handle the download to GCS.
     storage_client = storage.Client()
 
+    # Raises an error if the specified directory (dir_to_save) does not exist.
     try:
         bucket = storage_client.bucket(dir_to_save.split("/")[0])
     except:
@@ -32,18 +38,22 @@ def scrape_webpage(event, context):
         f"The specified directory {dir_to_save} does not exist"
         )
 
-    # Extract relevant attributes from the pub/sub message
+    # Extracts the relevant attributes from the pub/sub message.
     zip_url = event["attributes"]["zip_path"]
     link = event["attributes"]["link_path"]
 
-    # Deal with the 'blob' notation
+    # The blob (binary large object) method creates the files. The
+    # name of the blob corresponds to the unique path of the object
+    # in the bucket.
     if len(dir_to_save.split("/")[1:]) > 0:
         blob = bucket.blob("/".join(dir_to_save.split("/")[1:]) + "/" + link)
     else:
         blob = bucket.blob(link)
     
+    # Makes a request to the zip_url and returns a sequence of bytes.
     print("Downloading " + link + "...")
     zip_file = requests.get(zip_url).content
     
+    # Saves the 'zip_files' by uploading the contents to the storage bucket.
     print("Saving zip file " + link + "...")
     blob.upload_from_string(zip_file, content_type="application/zip")
